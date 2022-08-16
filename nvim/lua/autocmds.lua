c00fddc80b6cb2f077385442dbdc58fe39f23295
Iago Quintero <iagoquintero@letsbit.io>
2022-08-15 21:28:17 -0300

    autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,8 +1,29 @@
 vim.cmd([[
-autocmd BufWritePre *.go lua goimports()
-autocmd BufWritePre *.go lua vim.lsp.buf.formatting()
 autocmd TermOpen * setlocal nonumber norelativenumber
 ]])
 
-vim.api.nvim_create_augroup('bufcheck', {clear = true})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		vim.lsp.buf.formatting_sync(nil, 1000)
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.go" },
+	callback = function()
+		local params = vim.lsp.util.make_range_params(nil, "utf-16")
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end,
+})
 
